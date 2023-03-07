8f8ee09a32fb572959295f4298a6d9e834d7bf32
lstubdal <lotte.stubdal@gmail.com>
2023-03-07 14:13:23 +0000

    configure pug

--- app/app.js
+++ app/app.js
@@ -4,15 +4,18 @@ const express = require("express");
 // Create express app
 var app = express();
 
+app.set('view engine', 'pug');
+app.set('views', './app/views');
+
 // Add static files location
 app.use(express.static("static"));
 
 // Get the functions in the db.js file to use
 const db = require('./services/db');
 
-// Create a route for root - /
+// Create a route for root
 app.get("/", function(req, res) {
-    res.send("Hello world!");
+    res.render("index");
 });
 
 // Create a route for testing the db
