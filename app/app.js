50ddb459093ec2e4b54ae3becd1d7cc8c5cd15f0
lstubdal <lotte.stubdal@gmail.com>
2023-03-08 18:04:33 +0000

    Render homepage w/ PUG

--- app/app.js
+++ app/app.js
@@ -7,15 +7,19 @@ var app = express();
 app.set('view engine', 'pug');
 app.set('views', './app/views');
 
+// Add css styling
+/* app.use(express.static('styles', './app/styles')) */
+app.use(express.static('styles'))
+
 // Add static files location
 app.use(express.static("static"));
 
 // Get the functions in the db.js file to use
 const db = require('./services/db');
 
-// Create a route for root
+// Create a route for root - /
 app.get("/", function(req, res) {
-    res.render("index");
+    res.render("index", {'title':'My index page', 'heading':'My heading'});
 });
 
 // Create a route for testing the db
