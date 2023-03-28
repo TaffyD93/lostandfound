b5524dccc1762c0860e24efc097c38bb00d8162a
lstubdal <lotte.stubdal@gmail.com>
2023-03-28 13:44:16 +0100

    Remove lab example

--- app/app.js
+++ app/app.js
@@ -29,8 +29,6 @@ app.get("/", function(req, res) {
     })
 });
 
-
-
 // Create a route for userprofile
 // => "/userprofile/:username"
 // Create a route for root - /
@@ -42,18 +40,6 @@ app.get("/userprofile", function(req, res) {
     })
 });
 
-
-
-// Create a dynamic route for /hello/<name>, where name is any value provided by user
-// At the end of the URL
-// Responds to a 'GET' request
-app.get("/hello/:name", function(req, res) {
-    // req.params contains any parameters in the request
-    // We can examine it in the console for debugging purposes
-    console.log(req.params);
-    //  Retrieve the 'name' parameter and use it in a dynamically generated page
-    res.send("Hello " + req.params.name);
-});
 
 // Start server on port 3000
 app.listen(3000,function(){
