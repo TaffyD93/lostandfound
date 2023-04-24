fbdbaef6bf9a9e08cf3302a381f05f85ba0cab1d
lstubdal <lotte.stubdal@gmail.com>
2023-04-24 12:26:08 +0100

    Fix public folder structure

---
+++ static/public/script/newPost.js
@@ -0,0 +1,13 @@
+/********* MAKE NEW POST LOGIC HERE ********/
+
+// new post clicked 
+function makeNewPost() {
+    console.log('clicked')
+}
+
+const newPostButton = document.querySelector('#make-new-post-btn');
+newPostButton.addEventListener('click', makeNewPost);
+
+
+
+
