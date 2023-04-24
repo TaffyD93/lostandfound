e020d40a123edb83fed3efe59740d5d69eadf496
lstubdal <lotte.stubdal@gmail.com>
2023-04-24 11:31:15 +0100

    addeventlistener

---
+++ static/public/newPost.js
@@ -0,0 +1,14 @@
+/********* MAKE NEW POST LOGIC HERE ********/
+
+// new post clicked 
+// const newPostButton = document.querySelector('#make-new-post-btn');
+
+function makeNewPost() {
+    console.log('clicked')
+}
+
+document.querySelector('#make-new-post-btn').addEventListener('click', makeNewPost());
+
+
+
+
