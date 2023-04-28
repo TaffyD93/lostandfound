dd8cbcd2d4b03ba33bde63956f50c2a79b2be7bb
lstubdal <lotte.stubdal@gmail.com>
2023-04-28 18:58:55 +0100

    preview uploaded img before submitting form

--- static/public/script/newPost.js
+++ static/public/script/newPost.js
@@ -1,13 +1,29 @@
 /********* MAKE NEW POST LOGIC HERE ********/
 
+
 // new post clicked 
 function makeNewPost() {
     console.log('clicked')
 }
 
+// preview current uploaded image
+function previewUploadedImage() {
+    const file = input.files[0];
+    const reader = new FileReader();
+  
+    reader.addEventListener('load', function() {
+      preview.src = reader.result;
+    }, false);
+  
+    if (file) {
+      reader.readAsDataURL(file);
+    }
+}
+
 const newPostButton = document.querySelector('#make-new-post-btn');
 newPostButton.addEventListener('click', makeNewPost);
 
-
-
+const input = document.querySelector('#uploadImage');
+const preview = document.querySelector('#uploadedImage');
+input.addEventListener('change', previewUploadedImage) 
 
