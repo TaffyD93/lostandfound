7d80318da0349e8edeaeecd47c17ccf1d5224ed2
Altaf Dhanji <taffdhanji93@gmail.com>
2023-05-06 20:43:54 +0100

    auth

--- app/app.js
+++ app/app.js
@@ -13,6 +13,15 @@ var app = express();
 app.set('view engine', 'pug');
 app.set('views', './app/views');
 
+// Set the sessions
+var session = require('express-session');
+app.use(session({
+  secret: 'secretkeysdfjsflyoifasd',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}));
+
 // User import
 const { User } = require("../models/user");
 
@@ -43,7 +52,7 @@ app.post('/set-password', async function (req, res) {
             if (uId) {
                 // If a valid, existing user is found, set the password and redirect to the users single-student page
                 user.setUserPassword(params.password).then( result => {
-                    res.redirect('/userprofile');
+                    res.redirect('/userprofile/' + uId);
                 });
             }
             else {
@@ -63,6 +72,33 @@ app.get('/login', function (req, res) {
     res.render('login');
 });
 
+
+// Check submitted email and password pair
+app.post('/authenticate', async function (req, res) {
+    params = req.body;
+    var user = new User(params.email);
+    try {
+        uId = await user.getIdFromEmail();
+        if (uId) {
+            match = await user.authenticate(params.password);
+            if (match) {
+                req.session.uid = uId;
+                req.session.loggedIn = true;
+                res.redirect('/userprofile/' + uId);
+            }
+            else {
+                // TODO improve the user journey here
+                res.send('invalid password');
+            }
+        }
+        else {
+            res.send('invalid email');
+        }
+    } catch (err) {
+        console.error(`Error while comparing `, err.message);
+    }
+});
+
 // Create a route for root - /
 app.get("/", function(req, res) {
     var sql = 'select * from Posts';
@@ -75,7 +111,17 @@ app.get("/", function(req, res) {
 // Create a route for userprofile
 // => "/userprofile/:username"
 // Create a route for root - /
-app.get("/userprofile", function(req, res) {
+// app.get("/userprofile", function(req, res) {
+//     var sql = 'select * from Posts';
+//     db.query(sql).then(results => {
+//         // send results to index template
+//         res.render('userprofile', {data: results})
+//     })
+// });
+
+app.get('/userprofile/:uid', function(req, res) {
+    
+    console.log('you reach u/uid!!!!')
     var sql = 'select * from Posts';
     db.query(sql).then(results => {
         // send results to index template
